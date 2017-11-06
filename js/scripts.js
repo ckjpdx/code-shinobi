acb8aa7cb4afebd017ee02f53109b77a55f67a0a
Chris Johnson and Ginger Lee Kretschmer <chris@>
2017-11-06 13:50:01 -0800

    add flashcard name append to library list

--- js/scripts.js
+++ js/scripts.js
@@ -22,5 +22,12 @@ return isGuessCorrect;
 
 // FRONTEND
 $(function(){
-  
+  $('form#create-flashcard').submit(function(event){
+    event.preventDefault();
+    var flashName = $('input#flash-name').val();
+    var flashSnippet = $('input#flash-snippet').val();
+    var flashAnswer = $('input#flash-answer').val();
+    var flashcard = new Flashcard(flashName, flashSnippet, flashAnswer);
+    $('div#actual-list').children('ul').append('<li>' + flashName + '</li>');
+  });
 });
